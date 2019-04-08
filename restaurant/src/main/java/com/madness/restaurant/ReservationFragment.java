73043cc2a05d50e0e40d027bd8db587151bcacc2
Nicola <nicola.sabino94@gmail.com>
2019-04-08 23:47:43 +0200

    fucntional recycled view

--- restaurant/src/main/java/com/madness/restaurant/ReservationFragment.java
+++ restaurant/src/main/java/com/madness/restaurant/ReservationFragment.java
@@ -3,15 +3,29 @@ package com.madness.restaurant;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReservationFragment extends Fragment {
+
+
+    // fake content for list
+    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<String> identifiers = new ArrayList<>();
+    private ArrayList<String> seats = new ArrayList<>();
+    private ArrayList<String> dates = new ArrayList<>();
+    private ArrayList<String> time = new ArrayList<>();
 
 
     public ReservationFragment() {
@@ -22,8 +36,158 @@ public class ReservationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_reservation, container, false);
+        initElements();
+        RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(),names,identifiers,seats,dates,time);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(manager);
+        DividerItemDecoration decoration = new DividerItemDecoration(getContext(), manager.getOrientation());
+        recyclerView.addItemDecoration(decoration);
+        return rootView;
+
     }
+
+
+
+    private void initElements(){
+
+        names.add("Mario Rossi");
+        identifiers.add(String.valueOf(1));
+        seats.add(String.valueOf(4));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Luca Verdi");
+        identifiers.add(String.valueOf(2));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("21:00");
+
+        names.add("Giuseppe Neri");
+        identifiers.add(String.valueOf(3));
+        seats.add(String.valueOf(6));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Raffaella Rossi");
+        identifiers.add(String.valueOf(4));
+        seats.add(String.valueOf(1));
+        dates.add("8/04/2019");
+        time.add("19:00");
+
+        names.add("Bruno Verdi");
+        identifiers.add(String.valueOf(5));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("20:30");
+
+        names.add("Mario Rossi");
+        identifiers.add(String.valueOf(1));
+        seats.add(String.valueOf(4));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Luca Verdi");
+        identifiers.add(String.valueOf(2));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("21:00");
+
+        names.add("Giuseppe Neri");
+        identifiers.add(String.valueOf(3));
+        seats.add(String.valueOf(6));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Raffaella Rossi");
+        identifiers.add(String.valueOf(4));
+        seats.add(String.valueOf(1));
+        dates.add("8/04/2019");
+        time.add("19:00");
+
+        names.add("Bruno Verdi");
+        identifiers.add(String.valueOf(5));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("20:30");
+
+        names.add("Mario Rossi");
+        identifiers.add(String.valueOf(1));
+        seats.add(String.valueOf(4));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Luca Verdi");
+        identifiers.add(String.valueOf(2));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("21:00");
+
+        names.add("Giuseppe Neri");
+        identifiers.add(String.valueOf(3));
+        seats.add(String.valueOf(6));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Raffaella Rossi");
+        identifiers.add(String.valueOf(4));
+        seats.add(String.valueOf(1));
+        dates.add("8/04/2019");
+        time.add("19:00");
+
+        names.add("Bruno Verdi");
+        identifiers.add(String.valueOf(5));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("20:30");
+
+        names.add("Mario Rossi");
+        identifiers.add(String.valueOf(1));
+        seats.add(String.valueOf(4));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Luca Verdi");
+        identifiers.add(String.valueOf(2));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("21:00");
+
+        names.add("Giuseppe Neri");
+        identifiers.add(String.valueOf(3));
+        seats.add(String.valueOf(6));
+        dates.add("8/04/2019");
+        time.add("20:00");
+
+        names.add("Raffaella Rossi");
+        identifiers.add(String.valueOf(4));
+        seats.add(String.valueOf(1));
+        dates.add("8/04/2019");
+        time.add("19:00");
+
+        names.add("Bruno Verdi");
+        identifiers.add(String.valueOf(5));
+        seats.add(String.valueOf(2));
+        dates.add("8/04/2019");
+        time.add("20:30");
+
+    }
+
+   /* private void initRecycledView(){
+        RecyclerView recyclerView;
+        try{
+            recyclerView = getView().findViewById(R.id.recyclerView);
+            RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(),names,identifiers,seats,dates,time);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }*/
 
 }
